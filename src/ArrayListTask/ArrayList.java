ec3f01a3672a6036aeb60b576309d8a22eb0c481
YONA <alenastr12@gmail.com>
2018-04-26 01:48:40 +0300

    #012 ArrayList v.1.0

--- src/ArrayListTask/ArrayList.java
+++ src/ArrayListTask/ArrayList.java
@@ -1,13 +1,14 @@
 package ArrayListTask;
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.StringJoiner;
 
 public class ArrayList implements List {
     static final int INITIAL_CAPACITY = 5;
 
     Object[] array;
-    int size;
+    private int size;
 
     public ArrayList() {
         this(INITIAL_CAPACITY);
@@ -49,52 +50,82 @@ public class ArrayList implements List {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
-        return null;
+        array[index] = value;
+        Object updated = array[index];
+        return updated;
     }
 
     public void clear() {
-
+        array = Arrays.copyOf(array, 1);
+        array[0] = null;
+        size = 0;
     }
-
 
     public int size() {
-        return 0;
+        return size;
     }
-
 
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
-
 
     public boolean contains(Object value) {
-        return false;
+        boolean result = false;
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(value)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
-
 
     public int indexOf(Object value) {
-        return 0;
+        int result = 0;
+        if (!contains(value)) {
+            throw new NoSuchElementException("No such element");
+        }
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(value)) {
+                result = i;
+                break;
+            }
+        }
+        return result;
     }
-
 
     public int lastIndexOf(Object value) {
-        return 0;
+        int result = 0;
+        if (!contains(value)) {
+            throw new NoSuchElementException("No such element");
+        }
+        for (int i = size - 1; i >= 0; i++) {
+            if (array[i].equals(value)) {
+                result = i;
+                break;
+            }
+        }
+        return result;
     }
 
-
     public Object remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("No such index");
+        }
+        Object removed = array[index];
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        size--;
+        return removed;
     }
 
     // [A, B, C]
     public String toString() {
-            StringJoiner stringJoiner = new StringJoiner(",", "[", "]");
+        StringJoiner stringJoiner = new StringJoiner(", ", "[", "]");
         for (int i = 0; i < array.length; i++) {
             stringJoiner.add(array[i].toString());
         }
         return stringJoiner.toString();
     }
-
 
 }
 
--- test/ArrayListTask/ArrayListTest.java
+++ test/ArrayListTask/ArrayListTest.java
@@ -3,10 +3,13 @@ package ArrayListTask;
 import org.junit.*;
 import org.junit.rules.ExpectedException;
 
+import java.util.NoSuchElementException;
+
 import static junit.framework.Assert.assertNull;
 import static junit.framework.Assert.fail;
 import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertNotEquals;
 
 
 public class ArrayListTest {
@@ -14,47 +17,43 @@ public class ArrayListTest {
     Object actual;
     ArrayList list;
 
-    @Before
-    public void before() {
-
-    }
-
     @Test
     public void testAddByValueIntoEmptyArrayList() {
         list = new ArrayList(1);
-        assertNull(list.array[list.size]);
+        assertNull(list.array[list.size()]);
         Object shouldBeInserted = "some value";
         list.add(shouldBeInserted);
-        Object insertedValue = list.array[list.size - 1];
+        Object insertedValue = list.array[list.size() - 1];
         assertEquals(shouldBeInserted, insertedValue);
     }
 
     @Test
     public void testAddByValueCheckIncrementingListSize() {
         list = new ArrayList(1);
-        expected = list.size + 1;
+        expected = list.size() + 1;
         list.add(null);
-        actual = list.size;
+        actual = list.size();
         assertEquals(expected, actual);
     }
 
     @Test
-    public void testAddByValueIntoEndOfArrayList(){
+    public void testAddByValueIntoEndOfArrayList() {
         list = new ArrayList(2);
         list.add(null);
-        Object currentElementBeforeAdding = list.array[list.size - 1];
+        Object currentElementBeforeAdding = list.array[list.size() - 1];
         Object shouldBeInserted = "some value";
         list.add(shouldBeInserted);
-        Object previousElementAfterAdding = list.array[list.size - 2];
-        Object inserted = list.array[list.size - 1];
+        Object previousElementAfterAdding = list.array[list.size() - 2];
+        Object inserted = list.array[list.size() - 1];
         assertEquals(currentElementBeforeAdding, previousElementAfterAdding);
         assertEquals(shouldBeInserted, inserted);
     }
+
     @Test
-    public void testAddByValueCheckGrowingOfList(){
+    public void testAddByValueCheckGrowingOfList() {
         list = new ArrayList(1);
         list.add(1);
-        assertEquals(list.size, list.array.length);
+        assertEquals(list.size(), list.array.length);
         expected = list.array.length + 1;
         list.add(2);
         actual = list.array.length;
@@ -62,53 +61,181 @@ public class ArrayListTest {
     }
 
     @Test
-    public void testAddValueByIndexNegative() throws IndexOutOfBoundsException {
+    public void testAddByIndex() {
         list = new ArrayList(2);
-        //step1
-        try {
-            list.add("b",-1);
-            Assert.fail("Index should start at zero");
-        } catch (IndexOutOfBoundsException thrown) {
-            Assert.assertNotEquals("", thrown.getMessage());
-        }
-        //step2
-        try {
-            list.add("b",list.size);
-            Assert.fail("Index should be less count of elements");
-        } catch (IndexOutOfBoundsException thrown) {
-            Assert.assertNotEquals("", thrown.getMessage());
-        }
-    }
-
-
-    @Test
-    public void testAddValueByIndex() throws IndexOutOfBoundsException{
-        ArrayList list = new ArrayList(2);
         list.add(1);
-        list.add(2);
-        list.add(3);
-        //step1 adding into first cell
         int index = 0;
         Object shouldBeInserted = "a";
         Object shouldBeShifted = list.array[index];
-        int sizeShouldBeIncremented = list.size + 1;
-        list.add(shouldBeInserted,index);
+        list.add(shouldBeInserted, index);
         Object inserted = list.array[index];
         Object shifted = list.array[index + 1];
-        int sizeIncremented = list.size;
         assertEquals(shouldBeInserted, inserted);
         assertEquals(shouldBeShifted, shifted);
-        assertEquals(sizeShouldBeIncremented,sizeIncremented);
-        //step2 adding into last cell
-        index = list.size - 1;
-        shouldBeInserted = "a";
-        shouldBeShifted = list.array[index];
-        list.add(shouldBeInserted,index);
-        inserted = list.array[index];
-        shifted = list.array[index + 1];
-        assertEquals(shouldBeInserted, inserted);
-        assertEquals(shouldBeShifted, shifted);
+    }
 
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddByIndexNegativeBeforeStartIndex() {
+        list = new ArrayList(2);
+        list.add("b", -1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddByIndexNegativeAfterEndIndex() {
+        list = new ArrayList(2);
+        list.add("b", list.size());
+    }
+
+    @Test
+    public void testGet() {
+        list = new ArrayList(1);
+        expected = "some value";
+        list.add(expected);
+        actual = list.get(list.size() - 1);
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetNegativeBeforeStartIndex() {
+        list = new ArrayList(1);
+        list.get(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetNegativeAfterEndIndex() {
+        list = new ArrayList(1);
+        list.add(1);
+        list.get(list.size());
+    }
+
+    @Test
+    public void testSet() {
+        list = new ArrayList(1);
+        list.add(1);
+        Object beforeUpdate = list.get(0);
+        expected = "updated value";
+        list.set(expected, 0);
+        actual = list.get(0);
+        assertNotEquals(beforeUpdate, actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetNegativeBeforeStartIndex() {
+        list = new ArrayList(1);
+        list.set("", -1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetNegativeAfterEndIndex() {
+        list = new ArrayList(1);
+        list.add(1);
+        list.set("", list.size());
+    }
+
+    @Test
+    public void testClear() {
+        list = new ArrayList(1);
+        list.add(1);
+        assertEquals(1, list.size());
+        list.clear();
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void testSize() {
+        list = new ArrayList(1);
+        list.add(1);
+        assertEquals(1, list.size());
+        list.add(1);
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        list = new ArrayList(1);
+        assertEquals(true, list.isEmpty());
+        list.add(1);
+        assertEquals(false, list.isEmpty());
+    }
+
+    @Test
+    public void testContains() {
+        list = new ArrayList(1);
+        assertEquals(false, list.contains("some value"));
+        list.add("some value");
+        assertEquals(true, list.contains("some value"));
+    }
+
+    @Test
+    public void testIndexOf() {
+        list = new ArrayList(1);
+        list.add("some value");
+        list.add("some value");
+        assertEquals(0, list.indexOf("some value"));
+        assertNotEquals(1, list.indexOf("some value"));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testIndexOfNegative() {
+        list = new ArrayList(1);
+        list.add("1");
+        list.indexOf("some value");
+    }
+
+    @Test
+    public void testLastIndexOf() {
+        list = new ArrayList(1);
+        list.add("some value");
+        list.add("some value");
+        assertEquals(1, list.lastIndexOf("some value"));
+        assertNotEquals(0, list.lastIndexOf("some value"));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testLastIndexOfNegative(){
+        list = new ArrayList(1);
+        list.add("1");
+        list.lastIndexOf("some value");
+    }
+
+    @Test
+    public void testRemove() {
+        list = new ArrayList(1);
+        list.add("for remove");
+        list.add("some value");
+        int size = list.size();
+        Object removing = list.get(0);
+        Object nextAfterRemoving = list.get(1);
+        Object removed = list.remove(0);
+        Object current = list.get(0);
+        assertEquals(removing, removed);
+        assertEquals(current, nextAfterRemoving);
+        assertEquals(size - 1, list.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveNegativeBeforeStartIndex() {
+        list = new ArrayList(1);
+        list.remove( -1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveNegativeAfterEndIndex() {
+        list = new ArrayList(1);
+        list.add(1);
+        list.remove(list.size());
+    }
+
+    @Test
+    public void testToString() {
+        list = new ArrayList(1);
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        expected = "[A, B, C]";
+        actual = list.toString();
+        assertEquals(expected, actual);
     }
 
 }
